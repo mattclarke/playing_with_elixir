5689fc547eaeb09c888f7e51d5d808a6e8dd8852
Matt Clarke <matt.clarke@ess.eu>
2023-05-28 14:06:08 +0200

    starting todo list

---
+++ streams_examples/file_streams.exs
@@ -0,0 +1,49 @@
+defmodule Utils do
+  def large_lines!(path) do
+    File.stream!(path)
+      |> Stream.map(&String.replace(&1, "\n", ""))
+      |> Enum.filter(&(String.length(&1) > 80))
+  end
+
+  def line_lengths!(path) do
+    File.stream!(path)
+      |> Stream.map(&String.replace(&1, "\n", ""))
+      |> Stream.map(&(String.length(&1)))
+      |> Enum.to_list()
+  end
+
+  def longest_line_length!(path) do
+    File.stream!(path)
+      |> Stream.map(&String.replace(&1, "\n", ""))
+      |> Stream.map(&(String.length(&1)))
+      |> Enum.max()
+  end
+
+  def longest_line!(path) do
+    File.stream!(path)
+      |> Stream.map(&String.replace(&1, "\n", ""))
+      |> Enum.max_by(&String.length/1)
+  end
+
+  def words_per_line!(path) do
+    File.stream!(path)
+      |> Stream.map(&String.replace(&1, "\n", ""))
+      |> Stream.map(&(length(String.split(&1))))
+      |> Enum.to_list()
+  end
+end
+
+
+IO.puts(Utils.large_lines!("example.txt"))
+
+lengths = Utils.line_lengths!("example.txt")
+# Use inspect otherwise will get the list as chars
+IO.inspect(lengths, charlists: :as_lists)
+
+IO.puts(Utils.longest_line_length!("example.txt"))
+
+IO.puts(Utils.longest_line!("example.txt"))
+
+lengths = Utils.words_per_line!("example.txt")
+# Use inspect otherwise will get the list as chars
+IO.inspect(lengths, charlists: :as_lists)
---
+++ todo_list/simple_todo.ex
@@ -0,0 +1,23 @@
+defmodule TodoList do
+  def new(), do: %{}
+
+  def add_entry(todo_list, date, title) do
+    Map.update(
+      todo_list,
+      date,
+      [title],
+      fn titles -> [title | titles] end
+    )
+  end
+
+  def entries(todo_list, date) do
+    Map.get(todo_list, date, [])
+  end
+end
+
+
+my_list = TodoList.new()
+my_list = TodoList.add_entry(my_list, "a date", "do 1")
+my_list = TodoList.add_entry(my_list, "a date", "do 2")
+
+IO.puts("#{TodoList.entries(my_list, "a date")}")
