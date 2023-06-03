8c3c84afb45887f09d3c3147bd5ce291899ed102
Matt Clarke <matt.clarke@ess.eu>
2023-06-03 11:40:58 +0200

    added todo server

---
+++ todo_list/todo_server.ex
@@ -0,0 +1,100 @@
+defmodule TodoServer do
+  def start do
+    spawn(fn -> loop(TodoList.new()) end)
+  end
+
+  def add_entry(todo_server, new_entry) do
+    send(todo_server, {:add_entry, new_entry})
+  end
+
+  def entries(todo_server, date) do
+    send(todo_server, {:entries, self(), date})
+
+    receive do
+      {:todo_entries, entries} -> entries
+    after
+      5000 -> {:error, :timeout}
+    end
+  end
+
+  defp loop(todo_list) do
+    new_todo_list =
+      receive do
+        message -> process_message(todo_list, message)
+      end
+    loop(new_todo_list)
+  end
+
+  defp process_message(todo_list, {:add_entry, new_entry}) do
+    TodoList.add_entry(todo_list, new_entry)
+  end
+
+  defp process_message(todo_list, {:entries, caller, date}) do
+    send(caller, {:todo_entries, TodoList.entries(todo_list, date)})
+    todo_list
+  end
+end
+
+defmodule TodoList do
+  defstruct auto_id: 1, entries: %{}
+
+  def new(entries \\ []) do
+    Enum.reduce(
+      entries,
+      %TodoList{},
+      &add_entry(&2, &1)
+    )
+  end
+
+  def add_entry(todo_list, entry) do
+    entry = Map.put(entry, :id, todo_list.auto_id)
+
+    new_entries =
+      Map.put(
+        todo_list.entries,
+        todo_list.auto_id,
+        entry
+      )
+
+    %TodoList{todo_list | entries: new_entries, auto_id: todo_list.auto_id + 1}
+  end
+
+  def entries(todo_list, date) do
+    todo_list.entries
+    |> Stream.filter(fn {_, entry} -> entry.date == date end)
+    |> Enum.map(fn {_, entry} -> entry end)
+  end
+
+  def update_entry(todo_list, entry_id, updater_fun) do
+    case Map.fetch(todo_list.entries, entry_id) do
+      :error ->
+        todo_list
+
+      {:ok, old_entry} ->
+        old_entry_id = old_entry.id
+        # Ensure id doesn't change
+        new_entry = %{id: ^old_entry_id} = updater_fun.(old_entry)
+        new_entries = Map.put(todo_list.entries, new_entry.id, new_entry)
+        %TodoList{todo_list | entries: new_entries}
+    end
+  end
+
+  def update_entry(todo_list, %{} = new_entry) do
+    update_entry(todo_list, new_entry.id, fn _ -> new_entry end)
+  end
+
+  def delete_entry(todo_list, entry_id) do
+    entries = Map.delete(todo_list.entries, entry_id)
+    %TodoList{todo_list | entries: entries}
+  end
+end
+
+todo_server = TodoServer.start()
+TodoServer.add_entry(todo_server, %{date: ~D[2018-12-19], title: "Dentist"})
+TodoServer.add_entry(todo_server, %{date: ~D[2018-12-20], title: "Shopping"})
+TodoServer.add_entry(todo_server, %{date: ~D[2018-12-19], title: "Movies"})
+# TodoServer.update_entry(todo_server, 3, &Map.put(&1, :date, ~D[2018-12-20]))
+
+IO.inspect(TodoServer.entries(todo_server, ~D[2018-12-19]))
+IO.puts("---------------")
+IO.inspect(TodoServer.entries(todo_server, ~D[2018-12-20]))
