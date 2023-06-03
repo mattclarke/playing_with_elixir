b22b7cf2067430e0c204f4f51c3ff07082491de9
Matt Clarke <matt.clarke@ess.eu>
2023-06-03 14:02:26 +0200

    aoc22 did day 3

---
+++ aoc_22/day_3/solver.exs
@@ -0,0 +1,46 @@
+file = "input.txt"
+
+input_data =
+  File.stream!(file)
+  |> Stream.map(&String.replace(&1, "\n", ""))
+  |> Stream.map(&String.graphemes(&1))
+  |> Enum.to_list()
+
+convert_to_score = fn char ->
+  value = :binary.first(char)
+
+  if value >= :binary.first("a") do
+    value - :binary.first("a") + 1
+  else
+    value - :binary.first("A") + 27
+  end
+end
+
+find_match = fn comps ->
+  [front | back] = comps
+  front = MapSet.new(front)
+
+  common =
+    Enum.reduce(back, front, fn x, acc ->
+      MapSet.intersection(acc, MapSet.new(x))
+    end)
+
+  hd(MapSet.to_list(common))
+end
+
+data =
+  input_data
+  |> Stream.map(fn x -> Enum.chunk_every(x, trunc(length(x) / 2)) end)
+  |> Stream.map(&find_match.(&1))
+  |> Stream.map(&convert_to_score.(&1))
+  |> Enum.sum()
+
+IO.puts("Answer to part 1 = #{data}")
+
+data =
+  Enum.chunk_every(input_data, 3)
+  |> Stream.map(&find_match.(&1))
+  |> Stream.map(&convert_to_score.(&1))
+  |> Enum.sum()
+
+IO.puts("Answer to part 2 = #{data}")
