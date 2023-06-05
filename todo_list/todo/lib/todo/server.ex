51473311f358fa23a9c7b60bb18cc9834a14d658
Matt Clarke <matt.clarke@ess.eu>
2023-06-05 12:46:52 +0200

    first use of mix projects

---
+++ todo_list/todo/lib/todo/server.ex
@@ -0,0 +1,45 @@
+defmodule Todo.Server do
+  use GenServer
+
+  def start do
+    GenServer.start(Todo.Server, nil)
+  end
+
+  @impl GenServer
+  def init(_) do
+    {:ok, Todo.List.new()}
+  end
+
+  def add_entry(todo_server, new_entry) do
+    GenServer.cast(todo_server, {:add_entry, new_entry})
+  end
+
+  def entries(todo_server, date) do
+    GenServer.call(todo_server, {:entries, date})
+  end
+
+  def delete_entry(todo_server, entry_id) do
+    GenServer.cast(todo_server, {:delete_entry, entry_id})
+  end
+
+  @impl GenServer
+  def handle_cast({:add_entry, new_entry}, todo_list) do
+    new_state = Todo.List.add_entry(todo_list, new_entry)
+    {:noreply, new_state}
+  end
+
+  @impl GenServer
+  def handle_cast({:delete_entry, entry_id}, todo_list) do
+    new_state = Todo.List.delete_entry(todo_list, entry_id)
+    {:noreply, new_state}
+  end
+
+  @impl GenServer
+  def handle_call({:entries, date}, _, todo_list) do
+    {
+      :reply,
+      Todo.List.entries(todo_list, date),
+      todo_list
+    }
+  end
+end
