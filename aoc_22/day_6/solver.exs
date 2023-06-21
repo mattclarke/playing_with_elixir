0c3b138879d94e741bf48df37080b9964eedbb36
Matt Clarke <matt.clarke@ess.eu>
2023-06-21 07:46:12 +0200

    aoc6: done

---
+++ aoc_22/day_6/solver.exs
@@ -0,0 +1,30 @@
+file = "input.txt"
+
+input_data =
+  File.stream!(file)
+  |> Stream.map(&String.replace(&1, "\n", ""))
+  |> Stream.take(1)
+  |> Enum.at(0)
+  |> String.graphemes()
+
+defmodule Foo do
+  def solve(input_data, packet_size) do
+    {_, result} =
+      input_data
+      |> Stream.chunk_every(packet_size, 1, :discard)
+      |> Stream.with_index(packet_size)
+      |> Stream.filter(fn {x, _} ->
+        length(Enum.uniq(x)) == packet_size
+      end)
+      |> Stream.take(1)
+      |> Enum.at(0)
+
+    result
+  end
+end
+
+result = Foo.solve(input_data, 4)
+IO.puts("Answer to part 1 = #{result}")
+
+result = Foo.solve(input_data, 14)
+IO.puts("Answer to part 2 = #{result}")
