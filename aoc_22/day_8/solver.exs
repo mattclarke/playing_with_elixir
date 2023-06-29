b50114fdafb6d1bbdcfaee980406ad8f761d6d8e
Matt Clarke <matt.clarke@ess.eu>
2023-06-29 09:02:50 +0200

    aoc8: part 2

--- aoc_22/day_8/solver.exs
+++ aoc_22/day_8/solver.exs
@@ -11,7 +11,7 @@ input_data =
   |> Enum.to_list()
 
 defmodule Foo do
-  def solve(input_data) do
+  def solve1(input_data) do
     # From West
     {from_west, _} =
       input_data
@@ -60,8 +60,103 @@ defmodule Foo do
   defp from_west([], _, _, seen, best_row) do
     {seen, best_row}
   end
+
+  def solve2(tree_map) do
+    tree_map
+    |> Stream.map(fn {{col, row}, val} ->
+      l = go_left(tree_map, row, col - 1, val, 0)
+      r = go_right(tree_map, row, col + 1, val, 0)
+      u = go_up(tree_map, row - 1, col, val, 0)
+      d = go_down(tree_map, row + 1, col, val, 0)
+      l * r * u * d
+    end)
+    |> Enum.max()
+  end
+
+  defp go_left(tree_map, r, c, height, count) do
+    case c do
+      -1 ->
+        count
+
+      col ->
+        {_, next_tree} = Map.fetch(tree_map, {col, r})
+
+        if next_tree >= height do
+          count + 1
+        else
+          go_left(tree_map, r, col - 1, height, count + 1)
+        end
+    end
+  end
+
+  defp go_right(tree_map, r, c, height, count) do
+    case c do
+      99 ->
+        count
+
+      col ->
+        {_, next_tree} = Map.fetch(tree_map, {col, r})
+
+        if next_tree >= height do
+          count + 1
+        else
+          go_right(tree_map, r, col + 1, height, count + 1)
+        end
+    end
+  end
+
+  defp go_up(tree_map, r, c, height, count) do
+    case r do
+      -1 ->
+        count
+
+      row ->
+        {_, next_tree} = Map.fetch(tree_map, {c, row})
+
+        if next_tree >= height do
+          count + 1
+        else
+          go_up(tree_map, row - 1, c, height, count + 1)
+        end
+    end
+  end
+
+  defp go_down(tree_map, r, c, height, count) do
+    case r do
+      99 ->
+        count
+
+      row ->
+        {_, next_tree} = Map.fetch(tree_map, {c, row})
+
+        if next_tree >= height do
+          count + 1
+        else
+          go_down(tree_map, row + 1, c, height, count + 1)
+        end
+    end
+  end
 end
 
-result = Foo.solve(input_data)
+result = Foo.solve1(input_data)
 
 IO.puts("Answer to part 1 = #{MapSet.size(result)}")
+
+tree_map =
+  input_data
+  |> Stream.with_index()
+  |> Enum.reduce(%{}, fn {x, r}, acc1 ->
+    row_map =
+      x
+      |> Stream.with_index()
+      |> Enum.map(fn {y, c} ->
+        {{c, r}, y}
+      end)
+      |> Enum.into(%{})
+
+    Map.merge(acc1, row_map)
+  end)
+
+result = Foo.solve2(tree_map)
+
+IO.puts("Answer to part 2 = #{result}")
