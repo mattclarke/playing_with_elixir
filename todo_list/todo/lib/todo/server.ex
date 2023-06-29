8cc8c1815258cdf45a088d7f2927a7c5bd90982d
Matt Clarke <matt.clarke@ess.eu>
2023-06-29 13:52:49 +0200

    use genserver to expire

--- todo_list/todo/lib/todo/server.ex
+++ todo_list/todo/lib/todo/server.ex
@@ -1,14 +1,11 @@
 defmodule Todo.Server do
-  use Agent, restart: :temporary
+  use GenServer, restart: :temporary
+
+  @expiry_idle_timeout :timer.seconds(10)
 
   def start_link(name) do
-    Agent.start_link(
-      fn ->
     IO.puts("Starting to-do server for #{name}.")
-    {name, Todo.Database.get(name) || Todo.List.new()}
-      end,
-      name: via_tuple(name)
-    )
+    GenServer.start_link(Todo.Server, name, name: via_tuple(name))
   end
 
   defp via_tuple(name) do
@@ -17,21 +14,48 @@ defmodule Todo.Server do
 
   @impl GenServer
   def init(name) do
-    {:ok, {name, Todo.Database.get(name) || Todo.List.new()}}
+    {:ok, {name, Todo.Database.get(name) || Todo.List.new()}, @expiry_idle_timeout}
   end
 
   def add_entry(todo_server, new_entry) do
-    Agent.cast(todo_server, fn {name, todo_list} ->
-      new_list = Todo.List.add_entry(todo_list, new_entry)
-      Todo.Database.store(name, new_list)
-      {name, new_list}
-    end)
+    GenServer.cast(todo_server, {:add_entry, new_entry})
   end
 
   def entries(todo_server, date) do
-    Agent.get(
-      todo_server,
-      fn {_name, todo_list} -> Todo.List.entries(todo_list, date) end
-    )
+    GenServer.call(todo_server, {:entries, date})
+  end
+
+  def delete_entry(todo_server, entry_id) do
+    GenServer.cast(todo_server, {:delete_entry, entry_id})
+  end
+
+  @impl GenServer
+  def handle_cast({:add_entry, new_entry}, {name, todo_list}) do
+    new_state = Todo.List.add_entry(todo_list, new_entry)
+    Todo.Database.store(name, new_state)
+    {:noreply, {name, new_state}, @expiry_idle_timeout}
+  end
+
+  @impl GenServer
+  def handle_cast({:delete_entry, entry_id}, {name, todo_list}) do
+    new_state = Todo.List.delete_entry(todo_list, entry_id)
+    Todo.Database.store(name, new_state)
+    {:noreply, {name, new_state}, @expiry_idle_timeout}
+  end
+
+  @impl GenServer
+  def handle_call({:entries, date}, _, {name, todo_list}) do
+    {
+      :reply,
+      Todo.List.entries(todo_list, date),
+      {name, todo_list},
+      @expiry_idle_timeout
+    }
+  end
+
+  @impl GenServer
+  def handle_info(:timeout, {name, todo_list}) do
+    IO.puts("Stopping to-do server for #{name}")
+    {:stop, :normal, {name, todo_list}}
   end
 end
