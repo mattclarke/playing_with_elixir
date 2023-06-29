87a131c50ddee89e9c0845e724f6e64de027353f
Matt Clarke <matt.clarke@ess.eu>
2023-06-29 13:45:22 +0200

    use an agent

--- todo_list/todo/lib/todo/server.ex
+++ todo_list/todo/lib/todo/server.ex
@@ -1,9 +1,14 @@
 defmodule Todo.Server do
-  use GenServer, restart: :temporary
+  use Agent, restart: :temporary
 
   def start_link(name) do
+    Agent.start_link(
+      fn ->
     IO.puts("Starting to-do server for #{name}.")
-    GenServer.start_link(Todo.Server, name, name: via_tuple(name))
+    {name, Todo.Database.get(name) || Todo.List.new()}
+      end,
+      name: via_tuple(name)
+    )
   end
 
   defp via_tuple(name) do
@@ -16,37 +21,17 @@ defmodule Todo.Server do
   end
 
   def add_entry(todo_server, new_entry) do
-    GenServer.cast(todo_server, {:add_entry, new_entry})
+    Agent.cast(todo_server, fn {name, todo_list} ->
+      new_list = Todo.List.add_entry(todo_list, new_entry)
+      Todo.Database.store(name, new_list)
+      {name, new_list}
+    end)
   end
 
   def entries(todo_server, date) do
-    GenServer.call(todo_server, {:entries, date})
-  end
-
-  def delete_entry(todo_server, entry_id) do
-    GenServer.cast(todo_server, {:delete_entry, entry_id})
-  end
-
-  @impl GenServer
-  def handle_cast({:add_entry, new_entry}, {name, todo_list}) do
-    new_state = Todo.List.add_entry(todo_list, new_entry)
-    Todo.Database.store(name, new_state)
-    {:noreply, {name, new_state}}
-  end
-
-  @impl GenServer
-  def handle_cast({:delete_entry, entry_id}, {name, todo_list}) do
-    new_state = Todo.List.delete_entry(todo_list, entry_id)
-    Todo.Database.store(name, new_state)
-    {:noreply, {name, new_state}}
-  end
-
-  @impl GenServer
-  def handle_call({:entries, date}, _, {name, todo_list}) do
-    {
-      :reply,
-      Todo.List.entries(todo_list, date),
-      {name, todo_list}
-    }
+    Agent.get(
+      todo_server,
+      fn {_name, todo_list} -> Todo.List.entries(todo_list, date) end
+    )
   end
 end
